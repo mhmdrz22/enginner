7fba9d9b84585ac0f215bd3f2aca957fbbbffb82
mhmdrz22 <mhmdrdaqnbry2025@gmail.com>
2026-01-02 14:43:05 +0330

    Fix CI/CD, add Celery config, requirements update

--- backend/config/celery.py
+++ backend/config/celery.py
@@ -1,19 +1,17 @@
-from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
-from django.conf import settings
 
 # Set default Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-# Create Celery app
-app = Celery('config')
+app = Celery('taskboard')
 
 # Load config from Django settings with CELERY namespace
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Auto-discover tasks from installed apps
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.autodiscover_tasks()
+
 
 @app.task(bind=True, ignore_result=True)
 def debug_task(self):
