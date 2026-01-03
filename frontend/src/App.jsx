3672849e460a11f512cf56e5d613a0f76e42caf5
mhmdrz22 <mhmdrdaqnbry2025@gmail.com>
2026-01-03 22:16:40 +0330

    🐛 Fix JSX syntax error in App.jsx
    - Fix element prop on line 21: change from element=" to element={
    - This was causing vite build to fail with JSX parsing error
    - Now properly wraps PrivateRoute in curly braces

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
             <Route path="/register" element={<Register />} />
             <Route
               path="/dashboard"
-              element="
+              element={
                 <PrivateRoute>
                   <Dashboard />
                 </PrivateRoute>
