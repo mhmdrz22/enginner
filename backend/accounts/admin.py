558d5da0ba48d36e8c26d473d03b2a4d59feb8c5
mhmdrz22 <mhmdrdaqnbry2025@gmail.com>
2026-02-16 10:33:22 +0330

    fix: Update UserAdmin to work without username field
    - Remove all references to 'username' in list_display, fieldsets, and add_fieldsets
    - Add first_name and last_name fields to admin interface
    - Update fieldsets to match new User model structure
    - Add last_login_date to important dates
    - This fixes admin.E108 system check error

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,45 +1,90 @@
 from django.contrib import admin
-from .models import User
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User
 
 
 class UserAdminConfig(UserAdmin):
+    """Custom User Admin configuration without username field."""
+    
     model = User
-    search_fields = ("email",)
-    list_filter = ("email", "is_active", "is_staff")
+    
+    # Search and filtering
+    search_fields = ("email", "first_name", "last_name")
+    list_filter = ("is_active", "is_staff", "is_verified", "created_date")
     ordering = ("-created_date",)
-    list_display = ("email", "username", "is_active", "is_staff", "is_verified")
+    
+    # List display (removed username)
+    list_display = (
+        "email",
+        "get_full_name_display",
+        "is_active",
+        "is_staff",
+        "is_verified",
+        "created_date"
+    )
+    
+    # Fieldsets for viewing/editing existing users
     fieldsets = (
-        ("Authentication", {"fields": ("email", "username", "password")}),
-        ("Permissions", {"fields": ("is_staff", "is_active", "is_verified")}),
-        (
-            "Group Permissions",
-            {
-                "fields": (
-                    "groups",
-                    "user_permissions",
-                )
-            },
-        ),
-        ("Important dates", {"fields": ("last_login",)}),
+        (_("Authentication"), {
+            "fields": ("email", "password")
+        }),
+        (_("Personal Info"), {
+            "fields": ("first_name", "last_name")
+        }),
+        (_("Permissions"), {
+            "fields": (
+                "is_active",
+                "is_staff",
+                "is_superuser",
+                "is_verified"
+            )
+        }),
+        (_("Group Permissions"), {
+            "fields": (
+                "groups",
+                "user_permissions",
+            )
+        }),
+        (_("Important Dates"), {
+            "fields": (
+                "last_login",
+                "last_login_date",
+                "created_date",
+                "updated_date"
+            )
+        }),
     )
+    
+    # Fieldsets for adding new users
     add_fieldsets = (
-        (
-            None,
-            {
-                "classes": ("wide",),
-                "fields": (
-                    "username",
-                    "email",
-                    "password1",
-                    "password2",
-                    "is_active",
-                    "is_staff",
-                    "is_verified"
-                ),
-            },
-        ),
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email",
+                "first_name",
+                "last_name",
+                "password1",
+                "password2",
+                "is_active",
+                "is_staff",
+                "is_verified"
+            ),
+        }),
     )
+    
+    # Read-only fields
+    readonly_fields = (
+        "last_login",
+        "last_login_date",
+        "created_date",
+        "updated_date"
+    )
+    
+    def get_full_name_display(self, obj):
+        """Display full name in list view."""
+        return obj.get_full_name()
+    get_full_name_display.short_description = _("Full Name")
     
 
 admin.site.register(User, UserAdminConfig)
